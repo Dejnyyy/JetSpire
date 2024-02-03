e597b636d46fce6b157f77ef25de7a066092cb87
Dejnyyy <danslaby@email.cz>
2024-02-03 21:51:45 +0100

    abla

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,8 @@ export default function Home() {
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+             
+              <div className=" inset-y-0  flex items-center sm:hidden">
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open Menu</span>
@@ -29,10 +30,11 @@ export default function Home() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
                 <div className="flex flex-shrink-0 items-center">
                   <img
-                    className="h-8 w-auto"
+                    className="h-8 w-auto cursor-pointer"
                     src="https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/46gdLUn891cmJZxfzaRR/media/656b59cc0af0f785e2951c2d.png"
                     alt="JetSpire Logo"
                   />
@@ -44,7 +46,7 @@ export default function Home() {
                         key={item.name}
                         href={item.href}
                         className={clsx(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-gray-900 text-white font-mono font-semibold text-lg' : 'font-mono font-semibold text-lg text-gray-300 hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -55,18 +57,50 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
             </div>
           </div>
+          <section className='bg-black h-auto py-5'>
+            <div className='flex items-center justify-center'>
+              <div><h1 className='text-center text-6xl text-white font-mono font-semibold'>JETSPIRE</h1>
+              <h1 className='text-center text-2xl text-white font-mono font-semibold'>AI - Marketing</h1>
+              </div>
+              <img src="https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/46gdLUn891cmJZxfzaRR/media/656b59cc0af0f785e2951c2d.png"
+                    alt="JetSpire Logo"
+                    className='w-96 my-48'>
+              </img>
+            </div>
+          </section>
+          <section className='bg-black h-auto py-12'>
+              <div className='justify-center text-center flex-auto items-center mx-96'>
+                <strong className='text-white text-3xl font-mono font-semibold'>Boosting You Beyond The Stratosphere!</strong>
+                <h3 className="text-white text-xl font-mono font-semibold my-5  rounded-lg p-5 px-24">Welcome to Jetspire, your indispensable ally for scaling the elusive peaks of online visibility. We don't just sprinkle a few SEO keywords and call it a day! Our AI, tailored to your needs, crafts ingenious solutions helping you ascend to the zenith of the Google ranks - no ads attached.</h3>
+              </div>
+          </section>
+          <section className='bg-#8da9c4 h-auto py-12'>
+              <div className='justify-center text-center flex-auto items-center mx-96'>
+                <strong className='text-white text-4xl font-mono font-semibold'>Satisfied Customers</strong>
+                <div className="columns-3 mt-12">
+                <div className="w-full border rounded-lg p-5">
+                  <strong className='text-white text-2xl font-mono font-semibold'>"Discoverability Boost"</strong>  
+                  <p className="text-white text-lg font-mono px-3 mt-3">Since partnering with this SEO-savvy team, our business is now more visible on Google. It's not just numbers—it's real clicks and connections that are driving our success.</p>
+                  <h1 className="text-white text-xl font-mono font-semibold mt-3">- Propellr software</h1>
+                </div>  
+                <div className="w-full border rounded-lg p-5">
+                  <strong className='text-white text-2xl font-mono font-semibold'>"Right fit for our brand"</strong>
+                  <p className="text-white text-lg font-mono font px-3 mt-3">Thanks to this service, our website now shines higher on Google. It's more than numbers—it means more people finding us and choosing our offerings. Great tool for boosting online visibility!</p>
+                  <h1 className="text-white text-xl font-mono font-semibold mt-3">- KoelR</h1>
+                </div>
+                <div className="w-full border rounded-lg p-5">
+                  <strong className='text-white text-2xl font-mono font-semibold'>"From click to connection"</strong>
+                  <p className="text-white text-lg font-mono font px-3 mt-3">We've seen a positive shift in our online experience, all thanks to the improved Google ranking of our go-to brand. It's like they've become even more attuned to what we're looking for, making every click count.</p>
+                  <h1 className="text-white text-xl font-mono font-semibold mt-3">- FAI marketing</h1>
+                </div>
+              </div>  
+          </div>
+          </section>
+
+
+
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
@@ -76,7 +110,7 @@ export default function Home() {
                   as="a"
                   href={item.href}
                   className={clsx(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-gray-900 text-white font-mono font-semibold text-lg' : 'text-gray-300 hover:bg-gray-700 hover:text-white font-mono font-semibold text-lg',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
@@ -89,5 +123,6 @@ export default function Home() {
         </>
       )}
     </Disclosure>
+   
   )
-}
+} 
