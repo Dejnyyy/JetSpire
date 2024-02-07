cb9d2bf95648d6fcc2c1b0b6c4d8697e22c28c34
Dejnyyy <danslaby@email.cz>
2024-02-07 08:00:32 +0100

    wa

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -88,61 +88,61 @@ export default function About() {
             </div>
           </section>
 
-          <section className="h-auto bg-black py-8">
-            <div className="flex-auto  items-center justify-center p-4  ">
+          <section className="h-auto bg-black py-8 w-3/4 md:w-1/2 mx-auto">
+            <div className="flex-auto  items-center justify-center ">
               <h1 className=" font-mono text-xl font-semibold text-white sm:text-2xl  md:text-4xl lg:text-6xl">
                 How do we boost you beyond the stratosphere?
               </h1>
-              <h3 className="my-3 p-3  font-mono  text-xl font-semibold text-white">
+              <h3 className="my-3  font-mono  text-xl  text-white">
                 Welcome to Jetspire, where we redefine online visibility with
                 cutting-edge AI-based marketing tools. One of our standout
                 offerings is Programmatic SEO — a strategic approach that
                 maximizes your reach and impact in the digital realm. Let's
                 delve into how it works:
               </h3>
-              <h3 className=" p-3 border-t border-white font-mono text-xl font-semibold text-white">
+              <h3 className="  border-t border-white font-mono text-xl font-semibold text-white">
                 1. Strategic Website Setup
               </h3>
-              <p className="p-5 font-mono text-lg text-white">
+              <p className=" pt-2 font-mono text-lg text-white">
                 We create a dynamic website acting as a comprehensive directory,
                 meticulously optimized for SEO. Leveraging data from US or UK
                 Census, we generate SEO-optimized pages for each keyword we
                 target and every town or village within our specified area.
               </p>
-              <h3 className="p-3 border-t border-white font-mono text-xl font-semibold text-white">
+              <h3 className=" border-t border-white font-mono text-xl font-semibold text-white">
                 2. Population and Demographics Integration
               </h3>
-              <p className="p-5  font-mono text-lg text-white">
+              <p className=" pt-2  font-mono text-lg text-white">
                 The number of pages directly correlates with the number of
                 keywords and towns/villages. For instance, if we target "pest
                 control" with 3 keywords and 100 towns/villages, we generate 300
                 pages. Each page is tailored to a specific combination, such as
                 "pest control Round Rock Texas."
               </p>
-              <h3 className=" p-3 border-t  border-white font-mono text-xl font-semibold text-white">
+              <h3 className="  border-t  border-white font-mono text-xl font-semibold text-white">
                 3. Precision Optimization
               </h3>
-              <p className=" p-5  font-mono text-lg text-white">
+              <p className=" pt-2   font-mono text-lg text-white">
                 These pages are more than just content; they are fine-tuned for
                 SEO success. We employ meta tags and schema to optimize each
                 page and seamlessly link it to the business profile. Ideally,
                 this directory resides on a subdomain of your company website,
                 forging a strong association with Google.
               </p>
-              <h3 className=" p-3 border-t border-white font-mono text-xl font-semibold text-white">
+              <h3 className="  border-t border-white font-mono text-xl font-semibold text-white">
                 4. Analytics Integration
               </h3>
-              <p className=" p-5  font-mono text-lg text-white">
+              <p className=" pt-2   font-mono text-lg text-white">
                 The directory site is equipped with a separate Google Analytics
                 setup and Google Webmaster Tools. We proactively submit a
                 sitemap to Google, ensuring swift indexing of pages. Our goal is
                 to secure top 3 positions in search results for the targeted
                 keywords.
               </p>
-              <h3 className=" p-3 border-t border-white font-mono text-xl font-semibold text-white">
+              <h3 className="  border-t border-white font-mono text-xl font-semibold text-white">
                 5. User Engagement Tracking
               </h3>
-              <p className=" p-5  font-mono text-lg text-white">
+              <p className=" pt-2  font-mono text-lg text-white">
                 When users discover and engage with these pages, their
                 interactions are meticulously tracked through Google Analytics.
                 This engagement is crucial, especially when visitors are located
@@ -150,19 +150,19 @@ export default function About() {
                 both the keyword and location, sending strong signals to Google
                 that someone actively sought that specific service in that area.
               </p>
-              <h3 className=" p-3 border-t border-white font-mono text-xl font-semibold text-white">
+              <h3 className="  border-t border-white font-mono text-xl font-semibold text-white">
                 6. Closing the Loop with Reviews
               </h3>
-              <p className=" p-5  font-mono text-lg text-white">
+              <p className=" pt-2   font-mono text-lg text-white">
                 A positive loop is completed when engaged visitors, satisfied
                 with the information, leave reviews. This feedback significantly
                 enhances the impact on the local 3-pack results, establishing
                 your business prominently in local searches.
               </p>
-              <h3 className=" p-3 border-t border-white font-mono text-xl font-semibold text-white">
+              <h3 className="  border-t border-white font-mono text-xl font-semibold text-white">
                 Why Programmatic SEO Works
               </h3>
-              <p className=" p-5  font-mono text-lg text-white">
+              <p className=" pt-2 font-mono text-lg text-white">
                 Precision Targeting: By aligning content with specific keywords
                 and locations, we optimize your visibility where it matters
                 most.User-Centric Engagement: User interactions are actively
@@ -171,7 +171,7 @@ export default function About() {
                 Google's association between your business and the targeted
                 locations, elevating your online presence.
               </p>
-              <p className="rounded-lg p-5  font-mono text-lg text-white">
+              <p className=" pt-2rounded-lg font-mono text-lg text-white">
                 At Jetspire, Programmatic SEO isn't just a service; it's a
                 tailored approach to amplify your digital footprint. Let us
                 propel your business to the forefront of online visibility and
