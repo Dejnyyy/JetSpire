98a4ee85cac3687ceaabda6f0431acdb3618ecaf
Bagros <tom@bagros.eu>
2024-02-10 16:56:17 +0100

    uz na to nemam hovna

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -23,32 +23,34 @@ const Navbar = () => {
               </Disclosure.Button>
             </div>
             <Disclosure.Panel className="sm:hidden">
-              <div className="space-y-1 px-2 pb-3 pt-2">
-                <div className="flex">
-                  {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={
-                        "bg-gray-900 font-mono text-lg font-semibold text-white"
-                      }
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                </div>
+              <div>
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={
+                      "mx-2 font-mono text-lg font-semibold text-white"
+                    }
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
               </div>
             </Disclosure.Panel>
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
               <div className="flex flex-shrink-0 items-center">
-                <Image
-                  className="h-8 w-auto cursor-pointer"
-                  src="/logo.webp"
-                  alt="JetSpire Logo"
-                  width={1200}
-                  height={1200}
-                />
+                {!open && (
+                  <Link href="/">
+                    <Image
+                      className="h-8 w-auto cursor-pointer"
+                      src="/logo.webp"
+                      alt="JetSpire Logo"
+                      width={1200}
+                      height={1200}
+                    />
+                  </Link>
+                )}
               </div>
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
