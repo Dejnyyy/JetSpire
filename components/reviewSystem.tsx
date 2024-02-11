537ff4fbadd86f91c055b006546a4ad58b63bdec
Dejnyyy <danslaby@email.cz>
2024-02-11 22:56:13 +0100

    reviews system

---
+++ components/reviewSystem.tsx
@@ -0,0 +1,32 @@
+// components/reviewSystem.tsx
+import { reviews } from './reviews';
+
+interface ReviewProps {
+  index: number;
+}
+
+const Review: React.FC<ReviewProps> = ({ index }) => {
+  const review = reviews[index];
+  if (!review) {
+    return null; // Return null if the index is out of bounds
+  }
+  return (
+    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="flex space-x-4">
+        <div className="m-1 rounded-lg border-y py-2">
+          <h3 className="mt-3 p-1 font-semibold text-white md:text-xl xl:text-xl">
+            "{review.motto}"
+          </h3>
+          <p className="xl:text-md mt-3 px-3 text-sm text-white md:text-lg">
+            {review.text}
+          </p>
+          <h3 className="mt-3 text-white md:text-xl lg:text-2xl xl:text-lg">
+            - {review.name}
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
---
+++ components/reviews.tsx
@@ -0,0 +1,8 @@
+//components/reviews.tsx
+export const reviews = [
+    { motto: "Discoverability Boost", text: "Since partnering with this SEO-savvy team, our business is now more visible on Google. It's not just numbers—it's real clicks and connections that are driving our success.",name:"Propellr software" },
+    { motto: "Right fit for our brand", text: "Thanks to this service, our website now shines higher on Google. It's more than numbers—it means more people finding us and choosing our offerings. Great tool for boosting online visibility!",name:"KoelR" },
+    { motto: "From click to connection", text: "We've seen a positive shift in our online experience, all thanks to the improved Google ranking of our go-to brand. It's like they've become even more attuned to what we're looking for, making every click count.",name:"FAI marketing" },
+
+  ];
+  
